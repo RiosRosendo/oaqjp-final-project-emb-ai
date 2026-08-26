73acfc139176eefb64d71659ab735b910b78f1b9
RiosRosendo <delosriosrosendo@gmail.com>
2026-08-26 16:27:11 -0400

    Update emotion_detector to return formatted emotion dictionary

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -1,3 +1,4 @@
+import json
 import requests
 
 
@@ -9,4 +10,27 @@ def emotion_detector(text_to_analyze):
     myobj = {"raw_document": {"text": text_to_analyze}}
 
     response = requests.post(url, json=myobj, headers=headers)
-    return response.text
+
+    # Convertir la respuesta de texto JSON a un diccionario de Python
+    formatted_response = json.loads(response.text)
+
+    # Extraer las emociones de la estructura del JSON
+    emotions = formatted_response["emotionPredictions"][0]["emotion"]
+    anger_score = emotions["anger"]
+    disgust_score = emotions["disgust"]
+    fear_score = emotions["fear"]
+    joy_score = emotions["joy"]
+    sadness_score = emotions["sadness"]
+
+    # Encontrar la emoción dominante (la que tiene el puntaje más alto)
+    dominant_emotion = max(emotions, key=emotions.get)
+
+    # Retornar el diccionario con el formato solicitado
+    return {
+        "anger": anger_score,
+        "disgust": disgust_score,
+        "fear": fear_score,
+        "joy": joy_score,
+        "sadness": sadness_score,
+        "dominant_emotion": dominant_emotion,
+    }
